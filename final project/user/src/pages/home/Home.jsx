375389a56c6380eb00eb39cb6a8a2bf13978571b
Abhi-git-hub96 <117248866+Abhi-git-hub96@users.noreply.github.com>
2024-01-26 17:28:12 +0530

    Add files via upload

---
+++ final project/user/src/pages/home/Home.jsx
@@ -0,0 +1,175 @@
+import "./Home.css"
+
+
+
+
+export default function Home(){
+<script>
+
+const musicPlayer = document.getElementById('musicPlayer');
+    const playPauseButton = document.getElementById('playPauseButton');
+    function togglePlayPause(){
+      
+    }
+</script>
+
+    return(
+        <>
+<header>
+  <nav>
+    <div className="logo">
+    <img src="http://localhost:8800/images/music/Logo.png" alt="logo" width="180" height="90"></img>
+      
+    </div>
+    
+    
+    <div className="Menu">
+    
+    
+    <a href=" ">   Home       </a>
+    <a href=" ">  search      </a>
+    <a href=" ">  playlist    </a>
+    <a href=" ">  artists     </a>
+   
+    </div>
+  
+  </nav>
+  <div className="container">
+  <div className="songlist"></div>
+  <div className="songitemcontainer">
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\cover1.jpg" alt="1" ></img>
+    <span>Let me love you  -Justin Biber</span>
+    <span class="songlistplay"><span class="timestamp" >05:34  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\co2.jpg" alt="1" ></img>
+    <span>CO2  -Prateek Kuhad</span>
+    <span class="songlistplay"><span class="timestamp" >02:44  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <main>
+      <section>
+      
+        <h1>Welcome to Your Music App!<span class="change_content"> </span></h1>
+        
+        <p>"Discover the latest and greatest music. Explore new albums, find your favorite artists, and create playlists tailored just for you."</p>
+        <br></br>
+        <div className="songitem">
+    <img src="http://localhost:8800/images/music\Bawra-Mann-Dekhne-Ukulele-Chords-Swanand-Kirkire..jpg" alt="1" ></img>
+    <span>Bawra Mann  -Swanand Kirkire</span>
+    <span class="songlistplay"><span class="timestamp" >03:40  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\dholna song.jpg" alt="1" ></img>
+    <span>Dholna  -B praak</span>
+    <span class="songlistplay"><span class="timestamp" >02:54   </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\hass hass.jpg" alt="1" ></img>
+    <span>Hass Hass  -Diljit Dosanjh,Sia ,Greg</span>
+    <span class="songlistplay"><span class="timestamp" >02:44  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\sajan raazi.jpg" alt="1" ></img>
+    <span>Sajan Raazi  -Satinder Sartaaj</span>
+    <span class="songlistplay"><span class="timestamp" >05:13  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\waqt ki batien.jpg" alt="1" ></img>
+    <span>Waqt Ki Baate  -Dream Note</span>
+    <span class="songlistplay"><span class="timestamp" >04:58  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\A thousand year.jpg" alt="1" ></img>
+    <span>A Thousand Year  -Christina Perri</span>
+    <span class="songlistplay"><span class="timestamp" >04:48  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\Infinity-English-2019-20190325210151-500x500.jpg" alt="1" ></img>
+    <span>Infinity  -Jaymes young</span>
+    <span class="songlistplay"><span class="timestamp" >03:48  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+
+      </section>
+    </main>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\clam down.jpg" alt="1" ></img>
+    <span>Clam down  -Selena Gomez,Rema</span>
+    <span class="songlistplay"><span class="timestamp" >03:04 </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\closer.png" alt="1" ></img>
+    <span>Closer  -The Chainsmoker, Halsey</span>
+    <span class="songlistplay"><span class="timestamp" >04:07  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\one kiss.jpg" alt="1" ></img>
+    <span>One Kiss  -Dua Lipa, Calvin Harris</span>
+    <span class="songlistplay"><span class="timestamp" >03:44  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\Sean_Paul_ft_Dua_Lipa_-_No_lie.jpg" alt="1" ></img>
+    <span>No Lie  -Sean Paul, Dua Lipa</span>
+    <span class="songlistplay"><span class="timestamp" >03:49  </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\shree krishna govind.jpg" alt="1" ></img>
+    <span>Shree Krishna Govind  -Ravindra Jain</span>
+    <span class="songlistplay"><span class="timestamp" >07:40 </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    <div className="songitem">
+    <img src="http://localhost:8800/images/music\see you again.jpg" alt="1" ></img>
+    <span>See You Again  -Wiz KHalifa</span>
+    <span class="songlistplay"><span class="timestamp" >03:50 </span> <i class="fas fa-2x fa-play-circle"></i> </span>
+
+    </div>
+    
+
+  </div>
+
+  </div>
+  
+</header>
+
+<footer>
+      <h1><p>@copy; 2023 Your Music App. All rights reserved.</p></h1>
+    </footer>
+<div className="bottom">
+<input type="range" name="range" id="MyprogressBar" min="0" max="100"></input>
+<div className="icon">
+
+<div>
+      
+    </div>
+    <i class="fa-solid fa-backward"></i>
+            <i class="fa-solid fa-play" id="masterPlay"></i>
+            <i class="fas fa-3x fa-step-forward" id="next"></i> 
+
+</div>
+
+
+
+<div class="songInfo">
+            <img src="http://localhost:8800/images/playing gif.gif" width="42px" alt="" id="gif"></img> <span id="masterSongName">Warriyo - Mortals [NCS Release]</span>
+        </div>
+
+</div>
+<script src="https://kit.fontawesome.com/832a86a433.js" crossorigin="anonymous"></script>
+
+        </>
+    )
+}
